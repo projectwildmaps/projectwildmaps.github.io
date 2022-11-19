fa91f00e274bc95d867c5cbcf24ff527f2fb8935
Adam Kosinski <amk119@duke.edu>
2022-11-19 01:10:27 -0500

    Actually made autopan work well on a phone

--- autopan.js
+++ autopan.js
@@ -3,9 +3,9 @@ function autoPan(info_window_content){
     //to avoid our legend, which is fatter than the default controls
 
     let margin = { //min px margin from the info window
-        left: 23,
+        left: 11,
         right: 115,
-        top: window.innerWidth > 400 ? 73 : 113,
+        top: window.innerWidth > 400 ? 61 : 101,
         bottom: 33
     }
     // google's default margins are ish {left:61, right:79, top:61, bottom:83}
@@ -18,13 +18,18 @@ function autoPan(info_window_content){
 
     box = dialog_div.getBoundingClientRect();
 
-    let dx_needed_left = Math.min(0, box.x - margin.left);
-    let dx_needed_right = Math.max(0, box.right - (window.innerWidth - margin.right));
-    let dy_needed_top = Math.min(0, box.y - margin.top);
-    let dx_needed_bottom = Math.max(0, box.bottom - (window.innerHeight - margin.bottom));
+    let needed_left = Math.min(0, box.x - margin.left);
+    let needed_right = Math.max(0, box.right - (window.innerWidth - margin.right));
+    let needed_top = Math.min(0, box.y - margin.top);
+    let needed_bottom = Math.max(0, box.bottom - (window.innerHeight - margin.bottom));
 
-    let dx_pan = dx_needed_left + dx_needed_right; //eh it works
-    let dy_pan = dy_needed_top + dx_needed_bottom;
+    let dx_pan = needed_left + needed_right; //eh it works
+    let dy_pan = needed_top + needed_bottom;
+
+    console.log("left", needed_left)
+    console.log("right", needed_right)
+    console.log("top", needed_top)
+    console.log("bottom", needed_bottom)
 
     map.panBy(dx_pan, dy_pan);
 }
