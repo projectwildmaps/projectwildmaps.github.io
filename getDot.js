abeae8da6f8f2d279ceec1868c57a72b5f638589
Adam Kosinski <amk119@duke.edu>
2023-01-01 21:58:59 -0500

    All dots except archived are now svg, more configurable, larger click targets

--- getDot.js
+++ getDot.js
@@ -1,15 +1,16 @@
-function getDot(category) // Given the category of a data point, spits out the appropriate color of the data point
-{
-    switch(category){
-        case "Waterfall": return "dots/darkblue.png";
-        case "Water": ""; return "dots/blue.png";
-        case "Safety": return "dots/yellow.png";
-        case "Campsite": return "dots/lightgreen.png";
-        case "Pro-Tip!": return "dots/red.png";
-        case "Landmark": return "dots/green.png";
-        case "Funny Story": return "dots/pink.png";
-        case "Solos": return "dots/orange.png";
-        case "VAM (View Appreciation Moment)": return "dots/lightblue.png";
-        default: return "dots/white.png";
+function getDot(category, padding=0){
+    // Given the category of a data point, spits out an image svg data URL for the point
+    // Padding is the number of extra transparent px around the dot to make it easier to click a point on the map
+
+    if(!categories.includes(category)) {
+        category = "Other";
     }
+
+    let width = 2*dot_radius + 2*padding + 2; //+2 so we don't chop off the sides
+    let height = width;
+    let svg = 
+    `<svg version='1.1' width='${width}' height='${height}' xmlns='http://www.w3.org/2000/svg'>
+        <circle cx='${width/2}' cy='${height/2}' r='${dot_radius}' fill='${dot_colors[category]}' stroke='black' stroke-width='1.25' />
+    </svg>`;
+    return "data:image/svg+xml;utf8," + encodeURIComponent(svg);
 }
